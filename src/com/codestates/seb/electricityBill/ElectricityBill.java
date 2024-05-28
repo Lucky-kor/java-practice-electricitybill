7cce8f7ed7f7d7ade6e8b5a98ec30353cc67f0dd
dbwjdrbs <klolp2@naver.com>
2024-05-28 13:35:27 +0900

    유정균 전력계산기 입니다

--- src/com/codestates/seb/electricityBill/ElectricityBill.java
+++ src/com/codestates/seb/electricityBill/ElectricityBill.java
@@ -1,15 +1,19 @@
 package com.codestates.seb.electricityBill;
 
+import java.util.Scanner;
+
 public class ElectricityBill {
-  public static void main(String[] args) {
-    System.out.println("=".repeat(25));
-    System.out.println(" 주택용 전기요금(저압) 계산기 ");
-    System.out.println("=".repeat(25));
+    public static void main(String[] args) {
+        System.out.println("=".repeat(25));
+        System.out.println(" 주택용 전기요금(저압) 계산기 ");
+        System.out.println("=".repeat(25));
 
-    //TODO:
+        int powerUsage = 0;
+        System.out.print("전압 입력 -> ");
+        Scanner sc = new Scanner(System.in);
+        powerUsage = Integer.parseInt(sc.nextLine());
 
-    // 1, 100 이하인 경우 전력당 60.7 원이 부가됩니다. (사용한 전력 100 kWh 이하일 경)
-
-    // 2, 100 이하인 경우 전력당 60.7원 부과, 100 초과인 경우 125.9원 부과 (사용한 전력이 100 kWh 초과일 경우)
-  }
+        Calculation calc = new Calculation(powerUsage);
+        System.out.printf("%dkWh의 전기 요금은 %.1f원 입니다.\n", powerUsage, calc.getResult(powerUsage));
+    }
 }
